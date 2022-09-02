acccf13be59d326227b6e8e4d2cb3d32543329b7
Anand Kishore <anandpratyush272@gmail.com>
2022-09-02 12:20:01 +0530

    react-app

---
+++ README.md
@@ -0,0 +1,14 @@
+<div align="center">
+    <b><a href="" target="_blank">
+    </a></b>
+</div>
+
+# App Configuration
+
+Get Your OpenWeatherMap API KEY here: https://home.openweathermap.org/api_keys
+
+⚠️ **Important Instructions** ⚠️:
+
+- Create `.env` file in root of the project.
+- Copy the variable from [sample.env] and paste it into `.env`
+- Change the desired value. _eg_ API Key
---
+++ src/components/Footer.jsx
@@ -0,0 +1,51 @@
+import { useContext } from "react";
+import AppContext from "../provider/appContext";
+
+function Footer() {
+  const {
+    app: { isDark },
+  } = useContext(AppContext);
+  const anchorStyle = {
+    color: "#C6C6C6",
+  };
+  return (
+    <>
+      <footer className="footer">
+        <a
+          href="https://github.com/anandkishore06"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-brands fa-github"></i> <span>GitHub</span>
+        </a>
+        <a
+          href="https://github.com/anandkishore06"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-solid fa-code-fork"></i> <span>Fork</span>
+        </a>
+        <a
+          href="https://github.com/anandkishore06"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-solid fa-star"></i> <span>Star</span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anand-kishore-1550811a9/"
+          target="_blank"
+          rel="noreferrer"
+          style={isDark ? anchorStyle : null}
+        >
+          <i class="fa-brands fa-linkedin-in"></i> <span>Instagram</span>
+        </a>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
